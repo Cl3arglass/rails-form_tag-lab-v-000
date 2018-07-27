00b2e49b11515d0f068c375ffe254a6aa0573a4f
Holden Altaffer <haltaffer@gmail.com>
2018-07-27 03:47:37 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,13 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+  end
+
+  # def create
+  #   Post.create(title: params[:post][:title], description: params[:post][:description])
+  #   redirect_to posts_path
+  # end
 
 end
